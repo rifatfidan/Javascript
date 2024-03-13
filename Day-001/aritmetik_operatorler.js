cab41fca88eb5d013925f99b842dd54d80e4b1f7
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-14 01:06:25 +0300

    Create aritmetik_operatorler.js

---
+++ Day-001/aritmetik_operatorler.js
@@ -0,0 +1,22 @@
+/*
+Aritmetiksel Operatörler
+Toplama
+Çıkarma
+Çarpma
+Bölme
+% => Kalan
+*/
+//Toplama İşlemi
+var sayi1=52;
+var sayi2=50;
+console.log("Toplama İşleminin Sonucu :",sayi1+
+sayi2)
+//Çıkarma İşlemi
+console.log("Çıkarma İşleminin Sonuc :", sayi1-sayi2
+);
+//Bölme İşlemi
+console.log("Bölme İşleminin Sonucu ",sayi1/sayi2);
+//Çarpma İşlemi
+console.log("Çarpma İşleminin Sonucu ",sayi1*sayi2);
+//Kalan İşlemi
+console.log("Kalan İşleminin Sonucu ",sayi1%sayi2);
