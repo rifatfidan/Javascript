1a64d02762d6f4aae0ca28001cd99b24b653af49
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-13 23:14:50 +0300

    Create consolelog.js
    console log kullanımı

---
+++ Day-001/consolelog.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Console</title>
+    <script>
+        console.log("Js Kurs Hoşgeldiniz");
+        var e= "Rifat";
+        console.log(e);
+        var dersler = ["php","javascript","c#","python"];
+        console.log(dersler);
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
