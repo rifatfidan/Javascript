acd59b3837515f79b075243a7a0699fc0d132a89
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-14 01:15:49 +0300

    Create atama_operatorleri.js

---
+++ Day-001/atama_operatorleri.js
@@ -0,0 +1,22 @@
+/*
+Atama Operatörler
+==> atama operatörü
++= > toplar ve atama yapar
+-= > çıkartır ve atama yapar
+/  *= çarpar ve atama yapar
+/= => böler ve atama yapar
+%= kalanı verir ve atama yapar
+*/
+var e=10;
+var ad="Zubeyir Fidan";
+e+=20;
+console.log(e);
+e-=10;
+console.log(e);
+e*=10;
+console.log(e);
+e/=10;
+console.log(e);
+e%=10;
+console.log(e);
+console.log(ad);
