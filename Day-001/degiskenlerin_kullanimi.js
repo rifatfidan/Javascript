3f8b82bad9eeb6471cb1a466d8c5af087081e041
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-14 00:09:15 +0300

    Create degiskenlerin_kullanimi.js

---
+++ Day-001/degiskenlerin_kullanimi.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Javascript Kursu</title>
+<script type="text/javascript">
+/*
+Değişkenlere Giriş ve Yazım Kuralları
+1-Degisken isimleri birharf vaya isareti ile
+baslayabilirim
+2-Küçük büyük harf duyarlılığı vardır.
+3-255 Karakter uzunluğunda olabilir.
+4-Türkçe karakter bulunamaz. ş,ç,ö,
+5-Değişken ismi rakam ile başlamamalı
+*/
+var Emrah=10;
+console.log(Emrah);
+var emrah = "e";
+console.log(emrah);
+</script>
+</head>
+<body>
+
+
+</body>
+</html>
