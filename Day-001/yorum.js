a84281622c44b221b147d45354f8ce79f79fc569
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-13 23:31:02 +0300

    Create yorum.js

---
+++ Day-001/yorum.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Console</title>
+    <script>
+        /*
+        console.log("Js Kurs Hoşgeldiniz");
+        var e= "Rifat";
+        console.log(e);
+        var dersler = ["php","javascript","c#","python"];
+        console.log(dersler);
+        */
+       
+        //tek satır yorum satırı
+        /*
+            console.log("Js Öğreniyorum");
+            a=10;
+            b=20;
+            console.log(a+b);
+        */
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
