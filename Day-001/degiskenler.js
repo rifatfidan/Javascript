5175c5c50b4323bf57d567a6e6a6e1ec05dac5b7
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-14 00:50:25 +0300

    Create degiskenler.js

---
+++ Day-001/degiskenler.js
@@ -0,0 +1,20 @@
+/*
+Değişkenler
+Değişken Türleri
+1-String
+2-Number
+3-Boolean true, false
+4- Null => Boş
+5- Undefined =>
+*/
+
+var e="zubeyir";
+var say=10;
+var sonuc=true;
+var bos=null;
+var zubeyir;
+console.log(e+" Bu Bir :"+typeof e);
+console.log(say+" Bu Bir :"+typeof say);
+console.log(sonuc+" Bu bir :"+typeof sonuc);
+console.log(bos+" Bu bir :"+typeof bos);
+console.log(zubeyir+" Bu bir :"+typeof zubeyir);
