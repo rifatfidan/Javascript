4859ba62e8182a6939f40d244bc0eac87bcf93c7
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-14 13:14:13 +0300

    Create string.js

---
+++ Day-001/string.js
@@ -0,0 +1,20 @@
+// string operatörü
+var  ad="Zubeyir";
+var soyad="Fidan";
+var adSoyad= ad+" "+soyad;
+console.log(adSoyad);
+
+adSoyad+=" 2024";
+console.log(adSoyad);
+
+var yas=42;
+var okul="Anadolu Unv";
+var is="Woodelya";
+var tel="0555 555 55 55";
+
+console.log("Adım : "+ad);
+console.log("Soyadım : "+soyad);
+console.log("Yasim : "+yas);
+console.log("Okulum : "+okul);
+console.log("İsim : "+is);
+console.log("Telefonum : "+tel);
