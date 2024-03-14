ac5f55ded608b89b4707b4d894fe96a4ca9b61f7
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-14 12:55:11 +0300

    Create artirma_eksiltme.js

---
+++ Day-001/artirma_eksiltme.js
@@ -0,0 +1,16 @@
+/*
+Arttırma ve Eksiltme Operatörleri
+=> Değişkenin değerini 1 arttırır
+=> Değişkenin değerini 1 azaltır
+*/
+var say=10;
+say++; //Değişkenin değerini bir arttırdım.
+++say; //2.kullanım türü
+console.log(say);
+var say1=20;
+say1--;
+--say1;
+console.log(say1);
+for (i=100; i>=0; i--) {
+console.log(i);
+}
