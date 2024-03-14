10ee60ac54442a7f34525a47241fc52317b1d840
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-14 18:07:25 +0300

    Create for.js

---
+++ Day-001/for.js
@@ -0,0 +1,12 @@
+for(i=1; i<=6;i++){
+    document.write("<h"+i+">Başlık</h"+i+">");
+}
+
+for(i=1;i<=10;i++){
+    if (i%2==0) {
+        document.write(i + " sayısı çifttir<br>");
+    } else {
+        document.write(i + " sayısı tektir<br>");
+    }
+
+}
