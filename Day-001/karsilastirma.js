f5ae709a4b8250e06f43dc01b1da55f7c8e76015
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-14 13:00:06 +0300

    Create karsilastirma.js

---
+++ Day-001/karsilastirma.js
@@ -0,0 +1,16 @@
+/*
+Karşılaştırma Operatörleri
+== Esitse
+/  != Eşit Değilse
+=== Kesin Eşitse Aynıysa
+/  !== Kesin Eşit Değilse Aynı Değilse
+> Büyüksel
+< Küçükse
+>= Büyük Eşitse
+<= Küçük Eşitse
+*/
+var a=30;
+var b=30
+if (b>=a) {
+console.log("a Büyük yada eşit");
+}
