cfc513c21952497e8a99e32d9efdf6e182c517d0
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-18 18:50:27 +0300

    Create style_property.js

---
+++ Day-003/style_property.js
@@ -0,0 +1,7 @@
+<p id="sonuc">Merhaba Javscript</p>
+<input type="button" onclick="islem()" value="Değiştir">
+  
+  function islem(){
+    document.getElementById("sonuc").style.backgroundColor="navy";
+    document.getElementById("sonuc").style.color="#fff";
+}
