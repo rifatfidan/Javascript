60b2d71607211e31c51f11631917bd12c76f480f
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-18 15:34:38 +0300

    Create date.js

---
+++ Day-002/date.js
@@ -0,0 +1,8 @@
+
+var tarih= new Date();
+document.write("Bugün ayın "+tarih.getDate()+" si<br>");
+document.write("Bugün haftanın "+tarih.getDay()+" si<br>");
+document.write("Bugün yılın "+tarih.getMonth()+" si<br>");
+document.write("Bugün yıllardan "+tarih.getFullYear()+" <br>");
+// tarih.setDate gibi komutlarla tarihi değişebiliriz.
+
