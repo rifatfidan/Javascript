d7274dcb896c590f1657e7f2014f8e2b4503d6d2
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-18 18:10:40 +0300

    Create by_classname.js

---
+++ Day-003/by_classname.js
@@ -0,0 +1,3 @@
+<p class="sinif">Merhaba DOM a hoşgeldiniz</p>
+    <button onclick="console.log(document.getElementsByClassName('sinif')[0].textContent)">Class Bulma</button>
+    <!--direkt js kodunu click e yazabiliyoruz-->
