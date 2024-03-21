3bd99d322426db9a317a5e5c3b0c3b0f57cac910
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-22 02:37:17 +0300

    Create scrollBy.js

---
+++ Day-005/scrollBy.js
@@ -0,0 +1,16 @@
+<style>
+   div{
+      width:2500px;
+      height: 2500px;
+   }
+</style>
+</head>
+<body>
+
+   <button onclick="islem()" style="position: fixed;">İlerlet</button>
+   <div></div>
+
+
+function islem(){
+    window.scrollBy(100,100);
+}
