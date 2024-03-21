c5fa523bad9ee6e1438a0bb57d5ff7074b040116
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-21 15:59:55 +0300

    Create onfocus.js

---
+++ Day-004/onfocus.js
@@ -0,0 +1,8 @@
+<input onfocus="islem(this)" type="text" id="sec" placeholder="Bir tuşa basınız...">
+<div id="sonuc"></div>
+
+
+function islem(x){
+    x.style.background="yellowgreen";
+    x.style.color="white";
+}
