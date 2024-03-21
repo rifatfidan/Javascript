a61edee53e575cdc09a332380b61b7847c17da96
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-21 23:58:14 +0300

    Create outerWidth-outerHeight.js

---
+++ Day-005/outerWidth-outerHeight.js
@@ -0,0 +1,7 @@
+function islem(){
+    var w = window.outerWidth;
+    var h = window.outerHeight;
+    document.getElementById("sonuc").innerHTML= "Width : "+ w + " ve  Height : "+h + " dir.";
+}
+
+//araç çubukları ve kaydırma çubukları dahil çözünürlüğü verir.
