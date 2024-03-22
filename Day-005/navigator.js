2b535adcf7f18a7e7493338cf635464b3b30d3d8
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-22 03:22:55 +0300

    Create navigator.js

---
+++ Day-005/navigator.js
@@ -0,0 +1,5 @@
+document.write("Tarayıcı ismi : "+navigator.appName+"<br>");
+document.write("Cookie Kontrol: "+navigator.cookieEnabled+"<br>");
+document.write("Dil Kontrol: "+navigator.language+"<br>");
+document.write("İnternet Kontrol: "+navigator.onLine+"<br>");
+document.write("Useragent Kontrol: "+navigator.userAgent+"<br>");
