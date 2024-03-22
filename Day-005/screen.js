9b58515b51be260df6f0737cb086adce1018e25b
Rifat Fidan <73911894+rifatfidan@users.noreply.github.com>
2024-03-22 03:27:37 +0300

    Create screen.js

---
+++ Day-005/screen.js
@@ -0,0 +1,3 @@
+document.write("Toplam genişlik ve yükseklik : "+screen.width + " * " + screen.height+"<br>"); 
+document.write("Toplam genişlik ve yükseklik : "+screen.availWidth + " * " + screen.availHeight+"<br>"); 
+document.write("Bit derinilik : "+screen.colorDepth+"<br>"); 
